1a39c5685f1dc5fa4107b6257cb7323f597a6d73
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2020-05-28 19:37:15 +1000

    install directus sdk, generate initial services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DownloadComponent } from './components/download/download.component';
+import { UploadComponent } from './components/upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DownloadComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,

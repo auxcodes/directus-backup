8aa5011ce69834cbca958595571e1b4c08b9bb63
Borgosity <borgosity@gmail.com>
2020-05-31 17:58:54 +1000

    work on login, split client in service breaks cms services

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,8 +1,7 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router } from '@angular/router';
-import DirectusSDK from '@directus/sdk-js';
 import { ClientService } from '../../services/directus/client.service';
+import { PageStateService } from '../../services/page-state.service';
 
 @Component({
   selector: 'app-login-form',
@@ -11,42 +10,46 @@ import { ClientService } from '../../services/directus/client.service';
 })
 export class LoginFormComponent implements OnInit {
 
-  client: DirectusSDK = new DirectusSDK({
-    url: '',
-    project: ''
-  });
-
+  url: string;
+  project: string;
   email: string;
   password: string;
   errorMsg: string;
 
-  constructor(private ngZone: NgZone,
+  constructor(
     private clientService: ClientService,
-    private router: Router,
-    private titleService: Title) {
+    private titleService: Title,
+    private stateService: PageStateService) {
 
   }
 
   ngOnInit() {
-    this.clientService.logout();
+    if (this.stateService.currentTab.value === 'download') {
+      this.clientService.downloadLogout();
+    }
+    if (this.stateService.currentTab.value === 'upload') {
+      this.clientService.uploadLogout();
+    }
     this.titleService.setTitle('Login');
   }
 
   signIn() {
-    this.clientService.login({ email: this.email, password: this.password })
-      .then(() => {
-        this.password = '';
-        this.navigate(['/admin/#/login']);
-      })
-      .catch(error => {
-        this.errorMsg = 'Admin login attempt was unsuccessful';
-      });
-  }
-
-  navigate(commands: any[]) {
-    this.ngZone.run(() => this.router.navigate(commands))
-      .then()
-      .catch(error => console.error('Admin login: ', error.msg));
+    if (this.stateService.currentTab.value === 'download') {
+      this.clientService.downloadLogin(
+        { url: this.url, project: this.project, storage: sessionStorage },
+        { email: this.email, password: this.password }
+      )
+        .then(() => this.password = '')
+        .catch(() => this.errorMsg = 'Download login attempt was unsuccessful');
+    }
+    if (this.stateService.currentTab.value === 'upload') {
+      this.clientService.uploadLogin(
+        { url: this.url, project: this.project, storage: sessionStorage },
+        { email: this.email, password: this.password }
+      )
+        .then(() => this.password = '')
+        .catch(() => this.errorMsg = 'Upload login attempt was unsuccessful');
+    }
   }
 
 }
--- src/app/services/directus/client.service.ts
+++ src/app/services/directus/client.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import DirectusSDK, { LoginCredentials } from '@directus/sdk-js';
+import DirectusSDK, { LoginCredentials, ClientOptions } from '@directus/sdk-js';
 import { BehaviorSubject } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
 
@@ -8,21 +8,32 @@ import { AuthenticationService } from './authentication.service';
 })
 export class ClientService {
 
-  client: DirectusSDK = new DirectusSDK({
-    url: '',
-    project: '',
-    storage: sessionStorage
-});
-isCmsReady: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  downloadClient: DirectusSDK = new DirectusSDK({ url: '', project: '', storage: sessionStorage });
+  uploadClient: DirectusSDK = new DirectusSDK({ url: '', project: '', storage: sessionStorage });
 
-constructor(private authService: AuthenticationService) {
-}
+  downloadReady: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  uploadReady: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-async login(credentials: LoginCredentials) {
-    this.authService.login(this.client, credentials).then(() => this.isCmsReady.next(this.client.loggedIn));
-}
+  constructor(private authService: AuthenticationService) {
+  }
 
-logout() {
-    this.authService.logout(this.client);
-}
+  async downloadLogin(options: ClientOptions, credentials: LoginCredentials) {
+    this.downloadClient = new DirectusSDK(options);
+    this.authService.login(this.downloadClient, credentials).then(() => this.downloadReady.next(this.downloadClient.loggedIn));
+  }
+
+  async uploadLogin(options: ClientOptions, credentials: LoginCredentials) {
+    this.uploadClient = new DirectusSDK(options);
+    this.authService.login(this.uploadClient, credentials).then(() => this.downloadReady.next(this.uploadClient.loggedIn));
+  }
+
+  downloadLogout() {
+    this.authService.logout(this.downloadClient);
+    this.downloadReady.next(false);
+  }
+
+  uploadLogout() {
+    this.authService.logout(this.uploadClient);
+    this.uploadReady.next(false);
+  }
 }

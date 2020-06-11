7eaa1f5c0243ec0f16c8e905953d9c2532e5f2e3
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2020-06-11 20:10:01 +1000

    fix date format for save file name

--- src/app/services/utils/file-manager.service.ts
+++ src/app/services/utils/file-manager.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { saveAs } from 'file-saver';
 import { ProjectContentService } from '../../services/project-content.service';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,14 @@ export class FileManagerService {
 
   saveToFile() {
     const project = JSON.stringify(this.contentService.downloadedProject.value);
-    const filename = this.contentService.downloadedProject.value.name + Date();
+    const filename = this.contentService.downloadedProject.value.name + '-' + this.fileDate();
     const blob = new Blob([project], { type: 'text/plain' });
     saveAs(blob, filename);
+  }
+
+  fileDate(): string{
+    const dateString = (Date.prototype.getFullYear() + Date.prototype.getMonth() + Date.prototype.getDate() + 
+                        Date.prototype.getHours() + ':' + Date.prototype.getMinutes() + ':' + Date.prototype.getSeconds()).toString() ;
+    return dateString;
   }
 }
